3d36fa57afc9661528643447b1813b76978173f4
Sorin Vieriu <sorin@fundomate.com>
2023-11-05 19:05:53 +0100

    Play around

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,17 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import path from "node:path";
+import { defineConfig, UserConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/test/setup.js',
-  },
-});
+const configuration = {
+	plugins: [react()],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+			$lib: path.resolve(__dirname, "./src/lib"),
+			$components: path.resolve(__dirname, "./src/components"),
+		},
+	},
+};
+
+export default configuration;
